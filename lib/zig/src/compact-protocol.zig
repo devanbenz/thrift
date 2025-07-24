9561e5b8e69795f0cffa8a13a6a0faf116b4fc12
Devan <devandbenz@gmail.com>
2025-07-24 07:49:00 -0500

    feat: Working on compact protocol

--- lib/zig/src/compact-protocol.zig
+++ lib/zig/src/compact-protocol.zig
@@ -1,19 +1,24 @@
 const std = @import("std");
 const types = @import("types.zig");
+const transport = @import("transport.zig");
 
 pub const CompactProtocol = struct {
     _ttype: types.TType,
-
-    _stream: std.net.
+    _stream: transport.Transport,
 
     pub const DEFAULT_MAX_MESSAGE_SIZE: i32 = 100 * 1024 * 1024;
     pub const DEFAULT_MAX_FRAME_SIZE: i32 = 16384000;
     pub const DEFAULT_TBINARY_STRICT_READ: bool = false;
     pub const DEFAULT_TBINARY_STRICT_WRITE: bool = true;
 
-    pub fn init(allocator: std.mem.Allocator) CompactProtocol {
-        return .{ ._ttype = types.TType.init(allocator) };
+    pub fn init(allocator: std.mem.Allocator, stream: transport.Transport) CompactProtocol {
+        return .{ ._ttype = types.TType.init(allocator), ._stream = stream };
     }
 
-    pub fn read_byte(self: @This()) !u8 {}
+    fn read_byte(self: @This()) !u8 {
+        const buf = [_]u8{0};
+        try self._stream.read_byte(buf);
+
+        return buf[0];
+    }
 };
